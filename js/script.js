7e96d0ce8a99fd371dd7d2ab45c19d4f29a492b9
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-03 01:17:46 +0000

    added code for website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function myButtonClicked() {
+// This function tells the user what rated movies they can watch
+// input
+  const age = parseInt(document.getElementById("userAge").value)
+
+// process
+  if (age >= 17) {
+    // output
+    document.getElementById("answer").innerHTML = "You can watch rated R movies alone!"
+  } else if (age >= 13) {
+    // output
+    document.getElementById("answer").innerHTML = "You can watch rated PG-13 movies alone!"
+  } else if (age >= 5) {
+    // output
+    document.getElementById("answer").innerHTML = "You can watch rated G or PG movies alone!"
+  } else {
+    // output
+    document.getElementById("answer").innerHTML = "You are too young to watch any movies alone!"
+  }
+}
